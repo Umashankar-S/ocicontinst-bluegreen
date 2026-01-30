fceaf7058af71afae4b6ce300ec9b1eb4bf6828f
Umashankar-S <umashankar.sangaranarayanan@gmail.com>
2026-01-30 20:04:41 +0530

    vcn variable default updates

--- modules/vcn/variables.tf
+++ modules/vcn/variables.tf
@@ -10,13 +10,13 @@ variable  compartment_ocid {  }
 
 ################## vcn
 
-variable "vcn_name" { default =  "dev-vcn"}  
-variable "label_prefix" { default =  "np"} 
+variable "vcn_name" { default =  "bluegreen-vcn"}  
+variable "label_prefix" { default =  "dev"} 
 variable "vcn_cidr" { 
     type = list
    # default = ["10.100.0.0/23"] 
     }
-variable "vcn_dns_label" { default = "devvcnnp"}
+variable "vcn_dns_label" { default = "devblugrnvcn"}
 variable "enable_ipv6" { default = false }
 
 ################## subnet
@@ -35,16 +35,6 @@ variable private_apps_subnet_cidr  {
      }
 variable private_apps_subnet_display_name { default =  "privateapps"}  
 variable private_apps_subnet_dns_label  { default =  "privateappssn"}  
-
-
-
-
-# variable db_subnet_cidr  { 
-#     type = list 
-#     default  =  ["10.169.49.128/26"] 
-#      }
-# variable db_subnet_display_name { default =  "dbsubnet"}  
-# variable db_subnet_dns_label  { default =  "dbsn"}  
 
 
 
